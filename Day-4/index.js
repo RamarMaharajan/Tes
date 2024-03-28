e2753f664cbaf53261a46d345d6f962b9913431a
RamarMaharajan <mramar038@gmail.com>
2024-03-28 16:29:13 +0530

    day-4

---
+++ Day-4/index.js
@@ -0,0 +1,43 @@
+// function applyDiscount(discount){
+//     var cartAmount = 2000;
+//     cartAmount -= discount;
+//     console.log(cartAmount);
+// }
+// console.log(applyDiscount(500));
+
+// var assignName = function(){
+//     return "Ramar Maharajan";
+// };
+// console.log(assignName());
+
+// var toyAmount = function(discount){
+//     var cartValue =200;
+//     cartValue -= discount;
+// }
+// console.log(toyAmount(30));
+
+// var shape = {
+//     is2D: true,
+// };
+// var circle = {
+//     radius: 3,
+// };
+// var rectangle = {
+//     length: 1,
+//     breadth: 2,
+// };
+
+// console.log("Is circle a 2D shape?" + circle.is2D);
+// console.log("Radius of circle -" + circle.radius);
+
+// console.log("Is rectangle a 2D shape?" + rectangle.is2D);
+// console.log(
+//     "Length of rectangle =" +
+//     rectangle.length +
+//     ", Breadth of rectangle =" +
+//     rectangle.breadth
+// );
+
+class Animal {}
+
+let a1 = new Animal;
